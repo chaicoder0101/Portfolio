22e01d2eb26e0c377d56285fd3500ea9a586df52
Tenzin <gyurmey1835@gmail.com>
2020-01-09 00:55:35 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Main from './components/layout/main';
+import NavBar from './components/navbar/navBar';
+
 
 function App() {
 	return (
 		<Fragment>
 			<div className="container">
+				<NavBar/>
 				<Main />
 			</div>
 		</Fragment>
---
+++ src/components/navbar/navBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navBar = () => {
+    return (
+        <div>
+            ss
+        </div>
+    )
+}
+
+export default navBar
