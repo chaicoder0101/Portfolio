0e17e7fb14c9bd671f6ec31c7de514e1ea8440da
Tenzin <gyurmey1835@gmail.com>
2020-12-23 19:04:55 +0100

    changes

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,42 +4,43 @@ import '../../App.css';
 const Projects = () => {
 	return (
 		<Fragment>
-			<div className="projects ">
-				<div className="card" style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
-					<div className="card-body">
-						<img src={require('../../github.png')} className="card-img-top" alt="ServerSideApp" />
+			<div className='projects '>
+				<div className='card' style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
+					<div className='card-body'>
+						<img src={require('../../github.png')} className='card-img-top' alt='ServerSideApp' />
 
-						<h5 className="card-title">User Experience Sampling</h5>
-						<p className="card-text">A Context Aware Experience Sampling Server Side App.</p>
+						<h5 className='card-title'>User Experience Sampling</h5>
+						<p className='card-text'>A Context Aware Experience Sampling Server Side App.</p>
 						<a
-							href="https://github.com/gyurmey/User-experience-sampling-/tree/master/ServersideApp"
-							className="btn btn-primary">
+							href='https://github.com/gyurmey/Context-Aware-Experience-Sampling'
+							className='btn btn-primary'
+						>
 							GitHub
 						</a>
 					</div>
 				</div>
 
-				<div className="card" style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
-					<div className="card-body">
-						<img src={require('../../github.png')} className="card-img-top" alt="Voting App" />
-						<h5 className="card-title">University Polling App</h5>
-						<p className="card-text">
+				<div className='card' style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
+					<div className='card-body'>
+						<img src={require('../../github.png')} className='card-img-top' alt='Voting App' />
+						<h5 className='card-title'>University Polling App</h5>
+						<p className='card-text'>
 							A Secure E-Voting prototype inspired from Neuchâtel protocol with return codes
 						</p>
-						<a href="https://github.com/gyurmey/university-voting-web-app" className="btn btn-primary">
+						<a href='https://github.com/gyurmey/university-voting-web-app' className='btn btn-primary'>
 							GitHub
 						</a>
 					</div>
 				</div>
 
-				<div className="card" style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
-					<div className="card-body">
-						<img src={require('../../github.png')} className="card-img-top" alt="Info logger" />
-						<h5 className="card-title">Contact-Info logger</h5>
-						<p className="card-text">
+				<div className='card' style={{ flex: '0 1 15%', 'margin-top': '10rem', 'margin-left': '7rem' }}>
+					<div className='card-body'>
+						<img src={require('../../github.png')} className='card-img-top' alt='Info logger' />
+						<h5 className='card-title'>Contact-Info logger</h5>
+						<p className='card-text'>
 							A MERN stack application to add contact information with authentication.
 						</p>
-						<a href="https://github.com/gyurmey/contact-info-logger" className="btn btn-primary">
+						<a href='https://github.com/gyurmey/contact-info-logger' className='btn btn-primary'>
 							GitHub
 						</a>
 					</div>
