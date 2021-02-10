106894550e307408fba1b3eb509d1a226bf9ccbc
Tenzin <gyurmey1835@gmail.com>
2021-02-10 21:16:29 +0100

    testimonials

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Testimonials = () => {
 	return (
-		<div className="testimonials">
-			<p>
-				"He worked project oriented and independent according to specifications and learned the process workflow
-				(research and conception, logic design, layout design, programming). Tenzin Gyurmey was always friendly
-				and courteous to his colleagues. He fulfilled all tasks reliably and to our complete satisfaction. We
-				thank him for the work at Rapidfacture and wish Tenzin Gyurmey a good start in his professional life. We
-				would be happy to welcome Mr. Tenzin Gyurmey to our company again."
-			</p>
-			<span className="quote">
-				<h4>- Felix Furtmayr, CEO, Rapidfacture GmbH</h4>
-			</span>
+		<div className='container'>
+			<div className='testimonials'>
+				<p>
+					"He worked project oriented and independent according to specifications and learned the process
+					workflow (research and conception, logic design, layout design, programming). Tenzin Gyurmey was
+					always friendly and courteous to his colleagues. He fulfilled all tasks reliably and to our complete
+					satisfaction. We thank him for the work at Rapidfacture and wish Tenzin Gyurmey a good start in his
+					professional life. We would be happy to welcome Mr. Tenzin Gyurmey to our company again."
+				</p>
+				<span className='quote'>
+					<h4>- Felix Furtmayr, CEO, Rapidfacture GmbH</h4>
+				</span>
+			</div>
 		</div>
 	);
 };
